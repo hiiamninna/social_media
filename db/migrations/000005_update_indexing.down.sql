43e0953a0c6258a8d9f45e20f84234999d4e629c
hiiamninna <hiiamninna@github.com>
2024-03-23 07:09:26 +0700

    u: db migration

--- db/migrations/000005_update_indexing.down.sql
+++ db/migrations/000005_update_indexing.down.sql
@@ -1,1 +1,8 @@
--- TODO : create erd then analize the indexing or query optimize
+DROP INDEX index_hash_user_id;
+DROP INDEX index_hash_user_phone;
+DROP INDEX index_hash_user_email;
+DROP INDEX index_friend_user_id;
+DROP INDEX index_friend_added_by;
+DROP INDEX index_hash_post_id;
+DROP INDEX index_comment_id;
+DROP INDEX index_hash_comment_post_id;
