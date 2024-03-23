ee142a3268363d44b66cb7342e18ba2ebbd6c50c
hiiamninna <hiiamninna@github.com>
2024-03-23 11:38:13 +0700

    u: db migration

--- db/migrations/000005_update_indexing.down.sql
+++ db/migrations/000005_update_indexing.down.sql
@@ -1,8 +1,20 @@
-DROP INDEX index_hash_user_id;
-DROP INDEX index_hash_user_phone;
-DROP INDEX index_hash_user_email;
-DROP INDEX index_friend_user_id;
-DROP INDEX index_friend_added_by;
-DROP INDEX index_hash_post_id;
-DROP INDEX index_comment_id;
-DROP INDEX index_hash_comment_post_id;
+DROP INDEX index_users_id;
+DROP INDEX index_users_phone;
+DROP INDEX index_users_email;
+DROP INDEX index_users_total_firend;
+
+DROP INDEX index_friends_user_id;
+DROP INDEX index_friends_added_by;
+DROP INDEX index_friends_created_at;
+
+DROP INDEX index_posts_id;
+DROP INDEX index_posts_user_id;
+DROP INDEX index_posts_post;
+DROP INDEX index_posts_tags;
+DROP INDEX index_posts_created_at;
+
+DROP INDEX index_comments_id;
+DROP INDEX index_comments_user_id;
+DROP INDEX index_comments_post_id;
+DROP INDEX index_comments_comment;
+DROP INDEX index_comments_created_at;
--- db/migrations/000005_update_indexing.up.sql
+++ db/migrations/000005_update_indexing.up.sql
@@ -1,11 +1,20 @@
-CREATE INDEX index_hash_user_id ON users USING HASH (id);
-CREATE INDEX index_hash_user_phone ON users USING HASH (phone);
-CREATE INDEX index_hash_user_email ON users USING HASH (email);
+CREATE INDEX index_users_id ON users (id);
+CREATE INDEX index_users_phone ON users USING HASH (phone);
+CREATE INDEX index_users_email ON users USING HASH (email);
+CREATE INDEX index_users_total_firend ON users USING BRIN (total_friend);
 
-CREATE INDEX index_friend_user_id ON friends (user_id);
-CREATE INDEX index_friend_added_by ON friends (added_by);
+CREATE INDEX index_friends_user_id ON friends (user_id);
+CREATE INDEX index_friends_added_by ON friends (added_by);
+CREATE INDEX index_friends_created_at ON friends USING BRIN (created_at);
 
-CREATE INDEX index_hash_post_id ON posts USING HASH (id);
+CREATE INDEX index_posts_id ON posts (id);
+CREATE INDEX index_posts_user_id ON posts (user_id);
+CREATE INDEX index_posts_post ON posts USING HASH (post);
+-- CREATE INDEX index_posts_tags ON posts USING GIST (tags);
+CREATE INDEX index_posts_created_at ON posts USING BRIN (created_at);
 
-CREATE INDEX index_comment_id ON comments (id);
-CREATE INDEX index_hash_comment_post_id ON comments USING HASH (post_id);
+CREATE INDEX index_comments_id ON comments (id);
+CREATE INDEX index_comments_user_id ON comments (user_id);
+CREATE INDEX index_comments_post_id ON comments (post_id);
+CREATE INDEX index_comments_comment ON comments USING HASH (comment);
+CREATE INDEX index_comments_created_at ON comments USING BRIN (created_at);
